6c21431d1eaeef1f35f3cc506d812f6ed1d95881
Stephen LaPorte <stephen.laporte@gmail.com>
2017-07-04 13:23:19 -0700

    please subscribe to the new rss feed!

--- generate.py
+++ generate.py
@@ -2,13 +2,26 @@
 
 import codecs
 import argparse
+import time
+from datetime import datetime
+from email.Utils import formatdate
 
 import yaml
 import ashes
+from boltons.timeutils import UTC, isoparse
 
-DEFAULT_OUTPUT_NAME = './static/index.htm'
+DEFAULT_PAGE_NAME = './static/index.htm'
+DEFAULT_RSS_NAME = './static/feed.rss'
 DEFAULT_YAML = 'content.yaml'
-DEFAULT_TEMPALTE_NAME = 'index.dust'
+DEFAULT_PAGE_TMPL = 'index.dust'
+DEFAULT_RSS_TMPL = 'rss.dust'
+
+
+def to_rss_timestamp(dt_obj, to_utc=False):
+    if to_utc and dt_obj.tzinfo:
+        dt_obj = dt_obj.astimezone(UTC)
+    return formatdate(time.mktime(dt_obj.timetuple()))
+
 
 def parse_page_contents(docs):
     ret = {'sections': [],
@@ -24,26 +37,36 @@ def parse_page_contents(docs):
             ret['sections'].append(doc)
             continue
         if doc.get('title'):
+            if not doc.get('date_added'):
+                import pdb; pdb.set_trace()
+            doc['pub_date'] = to_rss_timestamp(doc['date_added'])
             ret['visualizations'].append(doc)
     return ret
 
-def main(content_name, output_name):
+
+def main(content_name, page_name, rss_name):
     with codecs.open(content_name, 'r') as file:
         contents = file.read()
     from_yaml = yaml.load_all(contents)
     page_dict = parse_page_contents(from_yaml)
+    page_dict['cur_utc'] = to_rss_timestamp(datetime.now())
     ashes_env = ashes.AshesEnv(['./templates'], keep_whitespace=True)
-    rendered = ashes_env.render('index.dust', page_dict)
-    with codecs.open(output_name, 'w', 'utf-8') as file:
-        file.write(rendered)
-    print 'successfully generated %s' % output_name
+    rendered_page = ashes_env.render(DEFAULT_PAGE_TMPL, page_dict)
+    with codecs.open(page_name, 'w', 'utf-8') as outfile:
+        outfile.write(rendered_page)
+    rendered_rss = ashes_env.render(DEFAULT_RSS_TMPL, page_dict)
+    with codecs.open(rss_name, 'w', 'utf-8') as outfile:
+        outfile.write(rendered_rss)
+    print 'successfully generated %s, %s' % (page_name, rss_name)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--content', help='input YAML file with content',
                          default=DEFAULT_YAML)
-    parser.add_argument('--output', help='output HTML file',
-                         default=DEFAULT_OUTPUT_NAME)
+    parser.add_argument('--page', help='output HTML file name',
+                         default=DEFAULT_PAGE_NAME)
+    parser.add_argument('--rss', help='output RSS file name',
+                         default=DEFAULT_RSS_NAME)
     args = parser.parse_args()
-    main(args.content, args.output)
+    main(args.content, args.page, args.rss)
